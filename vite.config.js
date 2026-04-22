324c26401431a8545927c6f5144b8229570a24c7
ZorosLC <allister.dcosta02@gmail.com>
2026-04-22 22:03:13 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: {
-    https: true,
-    host: true
-  }
+  plugins: [react()],
+  base: "/heads-up-game/"
 })
